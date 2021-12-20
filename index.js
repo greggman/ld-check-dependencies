54e53da36298976e680525bd9af6ae0254491bd8
Gregg Tavares <gman@chromium.org>
2021-12-20 10:44:00 -0800

    update for new results

--- index.js
+++ index.js
@@ -1,5 +1,15 @@
+function gt(value, num) {
+  if (typeof value === 'number') {
+    return value > num;
+  } else if (Array.isArray(value)) {
+    return value.length > num;
+  } else {
+    throw new Error('value is neither number nor array');
+  }
+}
+
 module.exports = function dependenciesNeedUpdating() {
   const childProcess = require('child_process');
   const result = JSON.parse(childProcess.execSync('npm install --dry-run --json').toString());
-  return result.added.length > 0 || result.updated.length > 0 || result.removed > 0;
+  return gt(result.added, 0) || gt(result.updated, 0) || gt(result.removed, 0);
 };
